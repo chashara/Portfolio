ed6c3ef7f4f332d381f4c9938af9dee695df2d06
Chashara Bandaranayake <chashara.bandaranayake@tuni.fi>
2026-02-11 03:27:15 +0200

    updated About Me component

---
+++ src/components/Aboutme.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import TechStack from "./TechStack";
+
+export default function AboutMe() {
+
+return (
+
+<section className="about-me">
+<h1> Hi, I am Chashara</h1>
+<p>I am a recent software engineering graduate passionate about building
+        both frontend and backend solutions. I enjoy working in agile teams
+        and constantly learning new technologies.
+        </p>
+<h2> My Skills and Tech Stack</h2>
+<TechStack />
+
+
+</section>
+
+
+
+
+);
+
+
+
+}
