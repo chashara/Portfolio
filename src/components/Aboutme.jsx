ed6d94ad61df3238def6bba3a12b85f13450998d
Chashara Bandaranayake <chashara.bandaranayake@tuni.fi>
2026-02-11 04:29:24 +0200

    updated color schemes

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import TechStack from "./Techstack";
+import TechStack from "./TechStack";
+import "./AboutMe.css";
+
 
 export default function AboutMe() {
 
 return (
 
 <section className="about-me">
+    <div className="about-container">
 <h1> Hi, I am Chashara</h1>
 <p>I am a recent software engineering graduate passionate about building
         both frontend and backend solutions. I enjoy working in agile teams
@@ -14,7 +17,7 @@ return (
 <h2> My Skills and Tech Stack</h2>
 <TechStack />
 
-
+</div>
 </section>
 
 
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -1,31 +1,25 @@
 import React from "react";
 
 export default function TechStack() {
-
-const techs = [
-
-    { name: "Java", color:"#f89820" },
-    { name: "Python", color: "#306998" },
-    { name: "JavaScript", color: "#f7df1e" },
-    { name: "React", color: "#61dafb" },
+  const techs = [
+    { name: "Java", color: "#f89820" },
+    { name: "Python", color: "#7d53bcff" },
+    { name: "JavaScript", color: "#f7df1e", textColor: "#111" },
+    { name: "React", color: "#61dafb", textColor: "#111" },
     { name: "Node.js", color: "#339933" },
     { name: "Git/GitHub", color: "#181717" },
     { name: "SQL", color: "#f29111" },
     { name: "Jira/Agile", color: "#0052cc" },
-];
+  ];
 
-return (
+  return (
     <div className="tech-stack">
       {techs.map((tech) => (
         <span
           key={tech.name}
           style={{
             backgroundColor: tech.color,
-            color: "white",
-            padding: "0.5rem 1rem",
-            borderRadius: "8px",
-            margin: "0.3rem",
-            display: "inline-block",
+            color: tech.textColor || "white",
           }}
         >
           {tech.name}
@@ -33,5 +27,4 @@ return (
       ))}
     </div>
   );
-
 }
